1bddc55d37cb71ff2b93a5d68f0a4df9e4be8566
dotNate <somersetnathaniel@gmail.com>
2021-07-28 19:37:48 +0100

    changed tailwind to version for create react app and added craco to project, configured tailwind

--- src/Components/Molecules/DashboardHeader/index.tsx
+++ src/Components/Molecules/DashboardHeader/index.tsx
@@ -2,7 +2,7 @@ import SqftLogo from "../../Atoms/SqftLogo";
 
 const DashboardHeader = (): JSX.Element => {
     return (
-        <header>
+        <header className="h-16">
             <SqftLogo />
         </header>
     )
