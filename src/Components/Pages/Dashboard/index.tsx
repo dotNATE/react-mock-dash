fcf0cc36612d9ee2d25ffe6ab76a8c0a1db2f52c
dotNate <somersetnathaniel@gmail.com>
2021-07-28 16:34:00 +0100

    project set up, removed junk files, added Component dir structure, added Dashboard Component

---
+++ src/Components/Pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = ():JSX.Element => {
+return (
+    <div>
+        <nav></nav>
+        <section></section>
+        <main></main>
+    </div>
+)
+}
+
+export default Dashboard;
